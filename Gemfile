32fd99a33a33e4519e1ddf37c72a918ea01a95fc
gustavo <gustavobrcn@hotmial.com>
2020-01-06 15:18:51 -0500

    Switched to postgress database

--- Gemfile
+++ Gemfile
@@ -4,11 +4,5 @@ source "https://rubygems.org" do
     gem "sinatra"
     gem "sinatra-flash"
     gem "wdm"
-    group :development do
-    gem 'sqlite3'
-  end
-
-  group :production do
     gem 'pg'
-  end
 end
--- server.rb
+++ server.rb
@@ -4,37 +4,35 @@ require "sinatra/flash"
 require './models'
 
 set :port, 3000
-set :database, {adapter: 'sqlite3', database: './smash_blog.sqlite3'}
+# set :database, {adapter: 'sqlite3', database: './smash_blog.sqlite3'}
+set :database, {adapter: 'postgresql', database: 'smash_blog'}
 enable :sessions
 
-get '/' do 
+get '/signup' do 
     erb :signup
 end
 
+post '/signup' do
+    @user = User.new(params[:user])
+    if @user.valid? & params[:password] == params["confirm_password"]
+        @user.save
+        redirect '/profile'
+    else
+        puts flash[:error] = @user.errors.full_messages
+        redirect '/signup'
+    end
+    puts params[:password]
+    erb :profile
+end
 
 post '/login' do
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
     given_password = params[:password]
     if user.password == given_password
         session[:user_id] = user.id
         redirect '/profile'
     else
     end
-end
-
-
-
-post '/' do
-    @user = User.new(params[:user])
-    if @user.valid? 
-        @user.save
-        redirect '/profile'
-    else
-        flash[:error] = @user.errors.full_messages
-        redirect '/signup'
-    end
-    puts params[:password]
-    erb :profile
 end
 
 get '/profile' do
@@ -58,3 +56,4 @@ end
 
 
 # $mains = ["Banjo & Kazooie", "Bayonetta", "Bowser", "Bowser Jr.", "Captain Falcon", "Charizard", "Chrom", "Cloud", "Corrin", "Daisy", "Dark Pit", "Dark Samsus", "Diddy Kong", "Donky Kong", "Dr. Mario", "Duck Hunt", "Falco", "Fox", "Ganondorf", "Greninja", "Hero", "Ice Climbers", "Ike", "Incineror", "Inkling", "Isabelle", "Ivysaur", "Jigglypuff", "Joke", "Ken", "King DeDe", "King K. Rool", "Kirby", "Link", "little Mac", "Lucario", "Lucas", "Lucina", "Luigi", "Mario", "Marth", "Mega Man", "Meta Knight", "Mewtwo", "Mii Brawler", "Mii Gunner", "Mii Swordfighter", "Mr. Game & Watch", "Ness", "Olimar", "Pac-Man", "Palutena", "Peach", "Pichu", "Pikachu", "Piranha Plant", "Pit", "Pokemon Trainer", "R.O.B.", "Richter", "Ridley", "Robin", "Rosalina & Luna", "Roy", "Ryu", "Samus", "Sheik", "Shulk", "Simon", "Snake", "Sonic", "Squirtle", "Terry", "Toon Link", "Villager", "Wario", "Wii Fit Trainer", "Wolf", "Yoshi", "Young Link", "Zelda", "Zero Suit Samus"]
+
