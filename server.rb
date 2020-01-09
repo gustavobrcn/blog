907bec2e9a9d9624056c526dfca453fa8ceef31d
gustavo <gustavobrcn@hotmial.com>
2020-01-09 15:00:31 -0500

    Users can search for other users. Username shows in the url route

--- server.rb
+++ server.rb
@@ -39,9 +39,8 @@ post '/login' do
     given_password = params["password"]
     if @user.password == given_password
         session[:username] = @user.username
-        pp session[:username]
-        pp @user.id
-        redirect "/profile"
+        session[:user_id] = @user.id
+        redirect "/profile/#{params["username"]}"
     else
         redirect '/login'
     end
@@ -50,50 +49,48 @@ end
 
 
 # ----------Making Posts on Profile----------
-get '/profile' do
-    puts params
-    puts session
-    @user = User.find_by(username: session[:username])
-    pp @user
-    session[:user_id] = @user.id
-    # @user_posts = Post.where(user_id: session[:user_id]).order(created_at: :desc)
-    erb :profile
-end
-
 post '/profile' do
-    @user = User.find_by(username: session[:username])
-    session[:user_id] = @user.id
-    @post = Post.new(content: params["content"], user_id: @user.id)
+@post = Post.new(content: params["content"], user_id: session[:user_id])
     if @post.valid?
         @post.save
-        # @user_posts = Posts.where(user_id: session[:user_id]).order(created_at: :desc)
-        # pp @user_posts
-        pp "this is from the post request"
-        redirect "/profile"
+        redirect "/profile/#{session[:username]}"
     end
+end
+
+get '/profile/:username' do
+    @user = User.find_by(username: session[:username])
+    session[:user_id] = @user.id
     erb :profile
 end
 
-
-get '/logout' do
-    session[:username] = nil
-    session[:user_id] = nil
-    # erb :login
-    redirect '/'
+# ----------Search for Users----------
+post '/user' do
+    @searched_user = User.find_by(username: params["searched_user"])
+    session[:searched_user] = @searched_user.username
+    if params["searched_user"] == session[:username]
+        redirect '/profile'
+    else
+        redirect "/user/#{params["searched_user"]}"
+    end
 end
 
+get '/user/:serched_user' do
+    @searched_user = User.find_by(username: session[:searched_user])
+    erb :user
+end
 
-get '/settings/:username' do
+# ----------User Account----------
+get '/account/:username' do
     erb :settings
 end
 
-post '/delete_account' do
+post '/account' do
     user = User.find_by(params['user_name'])
     user.destroy
     erb :signup
 end
 
-
+# ---------- The Hub ----------
 get '/feed' do
     @posts = Post.all
     erb "<% @posts.each do |post| %>
@@ -102,9 +99,14 @@ get '/feed' do
     <% end %>
     "
 end
-# post '/profile' do 
-#     erb :profile
-# end
+
+# ----------Log out----------
+get '/logout' do
+    session[:username] = nil
+    session[:user_id] = nil
+    redirect '/'
+end
+
 
 
 # $mains = ["Banjo & Kazooie", "Bayonetta", "Bowser", "Bowser Jr.", "Captain Falcon", "Charizard", "Chrom", "Cloud", "Corrin", "Daisy", "Dark Pit", "Dark Samsus", "Diddy Kong", "Donky Kong", "Dr. Mario", "Duck Hunt", "Falco", "Fox", "Ganondorf", "Greninja", "Hero", "Ice Climbers", "Ike", "Incineror", "Inkling", "Isabelle", "Ivysaur", "Jigglypuff", "Joke", "Ken", "King DeDe", "King K. Rool", "Kirby", "Link", "little Mac", "Lucario", "Lucas", "Lucina", "Luigi", "Mario", "Marth", "Mega Man", "Meta Knight", "Mewtwo", "Mii Brawler", "Mii Gunner", "Mii Swordfighter", "Mr. Game & Watch", "Ness", "Olimar", "Pac-Man", "Palutena", "Peach", "Pichu", "Pikachu", "Piranha Plant", "Pit", "Pokemon Trainer", "R.O.B.", "Richter", "Ridley", "Robin", "Rosalina & Luna", "Roy", "Ryu", "Samus", "Sheik", "Shulk", "Simon", "Snake", "Sonic", "Squirtle", "Terry", "Toon Link", "Villager", "Wario", "Wii Fit Trainer", "Wolf", "Yoshi", "Young Link", "Zelda", "Zero Suit Samus"]
